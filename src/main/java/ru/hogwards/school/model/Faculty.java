057d6a3822e8e3b4633f37d69c105572021f8500
Александра <bzzz18@yandex.ru>
2023-06-23 16:00:01 +0300

    check please

---
+++ src/main/java/ru/hogwards/school/model/Faculty.java
@@ -0,0 +1,61 @@
+package ru.hogwards.school.model;
+
+import java.util.Objects;
+
+public class Faculty {
+    private long id;
+    private String name;
+    private String colour;
+
+    public Faculty(long id, String name, String colour) {
+        this.id = id;
+        this.name = name;
+        this.colour = colour;
+    }
+
+    @Override
+    public String toString() {
+        return "Faculty{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", colour='" + colour + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Faculty faculty = (Faculty) o;
+        return id == faculty.id && Objects.equals(name, faculty.name) && Objects.equals(colour, faculty.colour);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, colour);
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+}
